85e5c97f54cff8835ce0c04426e4bb7b9f43378d
HamidMammadov <hamidvm@code.edu.az>
2022-03-16 13:31:23 +0400

    First Commit

---
+++ P128FirstTextEditor/script.js
@@ -0,0 +1,77 @@
+//Number
+// let a = 5;
+// console.log(a);
+
+// a =15;
+// console.log(a);
+
+// let b = 3;
+
+// let sum = a+b;
+// console.log(sum);
+
+// let name = prompt("Adinizi Daxil Edin:");
+
+// console.log("adiniz: "+name);
+
+// //String text seklinde
+// let surname = "Mamm";
+
+// //Boolean true or false
+// let isMarred = false;
+
+//Objec
+
+//Operators
+//Arichmetic
+// let a = 17;
+// let b = 8;
+
+// // let result = a + b;
+// // console.log(result);
+// // console.log(a + b);
+// // console.log(a - b);
+// // console.log(b ** a); //5*5*5
+// // console.log(a / b);
+// // console.log(a % b);
+// // a++; //a = a+1;
+// a--;//a = a-1;
+// a--;//a = a-1;
+// a--;//a = a-1;
+// console.log(a);
+
+//Assignment
+// let a = 3;
+// let b = a;
+// // a+=3//a=a+5
+// // a-=6//a=a-5
+// // a*=6//a=a*5
+// // a/=6//a=a*5
+// // a%=6//a=a%6
+// a**=3//a=a%6
+// console.log(a);
+
+//Comparison
+// let a = 15;
+// let b = 20;
+// let c = 10;
+
+// console.log(a == b)
+// console.log(a != b)
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+
+//Logical
+// let a = 15;
+// let b = 20;
+// let c = 10;
+
+// // console.log(a < b && a > c && b < c);
+// // console.log(a > b || a > c || b < c || a>=c);
+// console.log(!(a < b && b > c && c < a));
+// console.log(!(a > b || b < c || c > a));
+// console.log(a > b && b > c || a > c || c < b);
+// console.log(a < b || b < c || a < c && c > b);
+// console.log((a < b || b < c || a < c) && c < b);
